1c7e3ed68811becfc1d363fa88db0c534762e09b
fujiwarashi <sur.deux.cordes@gmail.com>
2020-11-10 00:12:33 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :exams
+  has_many :questions
+  has_many :results
+
   validates :name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
